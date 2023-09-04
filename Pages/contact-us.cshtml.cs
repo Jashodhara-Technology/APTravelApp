5471930d5660ce3f991e3d728b47020bcb239454
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2023-09-04 11:35:51 +0530

    Add project files.

---
+++ Pages/contact-us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APTravelApp.Pages
+{
+    public class contact_usModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/site-map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APTravelApp.Pages
+{
+    public class site_mapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
